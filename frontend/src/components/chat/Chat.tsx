bf782478bbb8b2dd05eb45251fe04e66704baf16
emrecan-y <emrecanyilmaz1997@gmail.com>
2025-01-15 16:22:47 +0100

    refactor structure and add key prop for complete rerender

--- frontend/src/components/chat/Chat.tsx
+++ frontend/src/components/chat/Chat.tsx
@@ -1,35 +1,35 @@
-import { useContext } from "react";
+import { useContext, useMemo } from "react";
 import { UserContext } from "../../App";
-import { ChatWindow, ChatWindowProps } from "./ChatWindow";
+import { ChatWindow } from "./ChatWindow";
 
 function Chat() {
   const userContext = useContext(UserContext);
 
-  const globaChat: ChatWindowProps = {
-    buttonText: "Global",
-    receiveDestinationTopic: "/topic/chat/global-chat",
-    receiveDestinationUser: "/user/queue/chat/global-chat",
-    sendDestination: "/app/chat/global-chat",
-    connectDestination: "/app/connect/global-chat",
-  };
+  const globaChat = (
+    <ChatWindow
+      buttonText={"Global"}
+      receiveDestinationTopic={"/topic/chat/global-chat"}
+      receiveDestinationUser={"/user/queue/chat/global-chat"}
+      sendDestination={"/app/chat/global-chat"}
+      connectDestination={"/app/connect/global-chat"}
+    />
+  );
+
+  const lobbyChat = (
+    <ChatWindow
+      key={userContext.userLobbyId}
+      buttonText={`Lobby${userContext.userLobbyId}`}
+      receiveDestinationTopic={`/topic/chat/lobby/${userContext.userLobbyId}`}
+      receiveDestinationUser={`/user/queue/chat/lobby/${userContext.userLobbyId}`}
+      sendDestination={`/app/chat/lobby/${userContext.userLobbyId}`}
+      connectDestination={`/app/connect/lobby/${userContext.userLobbyId}`}
+    />
+  );
 
   return (
     <div className="flex flex-row gap-x-1 items-end fixed bottom-0 right-0.5">
-      {userContext.userLobbyId !== -1 && (
-        <div className="flex flex-col items-end drop-shadow-2xl">
-          <ChatWindow
-            buttonText={`Lobby${userContext.userLobbyId}`}
-            receiveDestinationTopic={`/topic/chat/lobby/${userContext.userLobbyId}`}
-            receiveDestinationUser={`/user/queue/chat/lobby/${userContext.userLobbyId}`}
-            sendDestination={`/app/chat/lobby/${userContext.userLobbyId}`}
-            connectDestination={`/app/connect/lobby/${userContext.userLobbyId}`}
-          />
-        </div>
-      )}
-
-      <div className="flex flex-col items-end drop-shadow-2xl">
-        <ChatWindow {...globaChat} />
-      </div>
+      {userContext.userLobbyId !== -1 && <div className="flex flex-col items-end drop-shadow-2xl">{lobbyChat}</div>}
+      <div className="flex flex-col items-end drop-shadow-2xl">{globaChat}</div>
     </div>
   );
 }
