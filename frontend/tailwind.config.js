29c0646f6018fa82699126bcc547cb064c4c154e
emrecan-y <emrecanyilmaz1997@gmail.com>
2025-01-17 15:31:22 +0100

    add new styles for uno cards

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -10,6 +10,18 @@ export default {
         "game-main-dark": "#030712",
         "game-main-medium": "#787e8f",
         "game-main-light": "#e5e7eb",
+
+        "uno-red": "#c40c00",
+        "uno-blue": "#0849a3",
+        "uno-green": "#328a10",
+        "uno-yellow": "#e7d004",
+        "uno-black": "#000000",
+        "uno-white": "#e2e2e2",
+      },
+
+      dropShadow: {
+        "uno-small-text": "-2px 2px rgb(0, 0, 0)",
+        "uno-large-text": "-4px 4px rgb(0, 0, 0)",
       },
     },
   },
