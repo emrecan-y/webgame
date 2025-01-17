f0a539dcef26c0c1f7ca3554c68353727936e479
emrecan-y <emrecanyilmaz1997@gmail.com>
2025-01-17 15:30:58 +0100

    add missing card types

---
+++ frontend/src/models/unoCard.ts
@@ -0,0 +1,22 @@
+export type UnoCard = {
+  color: UnoCardColor;
+  cardType: UnoCardType;
+};
+
+export type UnoCardColor = "RED" | "BLUE" | "GREEN" | "YELLOW" | "BLACK";
+export type UnoCardType =
+  | "ZERO"
+  | "ONE"
+  | "TWO"
+  | "THREE"
+  | "FOUR"
+  | "FIVE"
+  | "SIX"
+  | "SEVEN"
+  | "EIGHT"
+  | "NINE"
+  | "DRAW_TWO"
+  | "SKIP"
+  | "REVERSE"
+  | "SELECT_COLOR"
+  | "DRAW_FOUR";
