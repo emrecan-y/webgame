e3070bed50ad288d2bf90ff56da6a71d2f81f13d
emrecan-y <emrecanyilmaz1997@gmail.com>
2025-01-31 08:33:42 +0100

    refactor code for better readability

--- frontend/src/components/landing/AnimatedBackground.tsx
+++ frontend/src/components/landing/AnimatedBackground.tsx
@@ -9,23 +9,23 @@ export function AnimatedBackground() {
     return Math.round(Math.random() * (max - min) + min);
   }
 
-  function injectKeyframes(angle: number, directionX: number) {
+  function injectKeyframe(angle: number, directionX: number) {
     const keyframeName = `fall-${angle}-${directionX}`;
     const style = document.createElement("style");
     style.innerHTML = `
       @keyframes ${keyframeName} {
         0% {
-          top: -20vh;
+          top: -250px;
           transform: rotate(0deg) ;
         }
         100% {
-          top: 150vh;
+          top: calc(100% + 250px);
           transform: rotate(${angle}deg) translateX(${directionX}vw);
         }
       }
     `;
     document.head.appendChild(style);
-    return style;
+    return { style, keyframeName };
   }
 
   useEffect(() => {
@@ -33,30 +33,29 @@ export function AnimatedBackground() {
       () => {
         if (containerRef.current) {
           const cardElement = document.createElement("div");
+
           const angle = getRandomNumber(-180, 180);
           const directionX = getRandomNumber(-20, 20);
-
-          const keyframeName = `fall-${angle}-${directionX}`;
+          const { style, keyframeName } = injectKeyframe(angle, directionX);
 
           cardElement.style.position = "absolute";
-          cardElement.style.top = `${getRandomNumber(0, 100)}vh`;
+          cardElement.style.top = "0vh";
           cardElement.style.left = `${getRandomNumber(0, 100)}vw`;
           cardElement.style.scale = `0.${getRandomNumber(40, 99)}`;
-          cardElement.style.animation = `${keyframeName} 12s linear `;
+          cardElement.style.animation = `${keyframeName} ${getRandomNumber(8, 12)}s linear `;
+          cardElement.style.filter = "drop-shadow(0px 0px 25px #000000)";
 
           const cardContent = document.createElement("div");
           createRoot(cardContent).render(<UnoCardTopViewDisplay />);
-
-          const injectedStyle = injectKeyframes(angle, directionX);
-
           cardElement.appendChild(cardContent);
+
           containerRef.current.appendChild(cardElement);
 
           cardElement.addEventListener("animationend", () => {
             if (containerRef.current) {
               containerRef.current.removeChild(cardElement);
             }
-            document.head.removeChild(injectedStyle);
+            document.head.removeChild(style);
           });
         }
       },
