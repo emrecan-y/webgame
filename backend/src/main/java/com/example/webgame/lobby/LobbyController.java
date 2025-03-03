5b3f90bb01c78c671f48c04ff422533a4eab470d
emrecan-y <emrecanyilmaz1997@gmail.com>
2025-03-03 15:06:51 +0100

    refactor return types to only return when request valid

--- backend/src/main/java/com/example/webgame/lobby/LobbyController.java
+++ backend/src/main/java/com/example/webgame/lobby/LobbyController.java
@@ -1,20 +1,19 @@
 package com.example.webgame.lobby;
 
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.annotation.SendToUser;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.example.webgame.record.GeneralPlayerRequest;
 import com.example.webgame.record.LobbyCreateRequest;
 
-@RestController
+@Controller
 @CrossOrigin(origins = "*")
 public class LobbyController {
 
@@ -26,32 +25,31 @@ public class LobbyController {
 		this.template = template;
 	}
 
-	@MessageMapping("/create-lobby")
-	@SendToUser("/queue/lobby/lobby-id")
-	public Integer createLobby(@Header("simpSessionId") String sessionId, LobbyCreateRequest request) throws Exception {
-		Optional<Lobby> lobby = this.lobbyService.createLobby(sessionId, request.lobbyPassword(), request.lobbySize());
-		if (lobby.isPresent()) {
-			this.template.convertAndSend("/topic/lobby-list", this.lobbyService.getLobbyList());
-			return lobby.get().getId();
-		} else {
-			throw new NoSuchElementException("SessionId doesn't exist.");
-		}
-	}
-
 	@MessageMapping("/lobby-list")
 	@SendToUser("/queue/lobby-list")
 	public List<Lobby> getLobbyList() {
 		return this.lobbyService.getLobbyList();
 	}
 
+	@MessageMapping("/create-lobby")
+	public void createLobby(@Header("simpSessionId") String sessionId, LobbyCreateRequest request) throws Exception {
+		Optional<Lobby> lobby = this.lobbyService.createLobby(sessionId, request.lobbyPassword(), request.lobbySize());
+		if (lobby.isPresent()) {
+			this.template.convertAndSend("/topic/lobby-list", this.lobbyService.getLobbyList());
+
+			// Inform user that the their lobby has changed
+			this.template.convertAndSend("/queue/lobby/lobby-id" + "-user" + sessionId, lobby.get().getId());
+		}
+	}
+
 	@MessageMapping("/lobby/add-player")
-	@SendToUser("/queue/lobby/lobby-id")
-	public Integer addPlayerToLobby(GeneralPlayerRequest request) {
+	public void addPlayerToLobby(@Header("simpSessionId") String sessionId, GeneralPlayerRequest request) {
 		if (this.lobbyService.addPlayerToLobby(request.lobbyId(), request.nickName(), request.lobbyPassword())) {
 			this.template.convertAndSend("/topic/lobby-list", this.lobbyService.getLobbyList());
-			return request.lobbyId();
+
+			// Inform user that the their lobby has changed
+			this.template.convertAndSend("/queue/lobby/lobby-id" + "-user" + sessionId, request.lobbyId());
 		}
-		return -1;
 	}
 
 }
