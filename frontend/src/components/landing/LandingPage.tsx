488c781e0d6716c01c10a1867dd83dc7a57da02e
emrecan-y <emrecanyilmaz1997@gmail.com>
2025-04-08 16:02:41 +0200

    feat(LandingPage): add loading screen for server connection

--- frontend/src/components/landing/LandingPage.tsx
+++ frontend/src/components/landing/LandingPage.tsx
@@ -7,8 +7,10 @@ import { motion } from "motion/react";
 import MotionButton from "../ui/MotionButton";
 import { ProfanityFilterContext } from "../context/ProfanityFilterContext";
 import PrivacyPolicy from "./PrivacyPolicy";
+import ErrorPopUp from "../error/ErrorPopUp";
 
 function LandingPage() {
+  const [isConnected, setIsConnected] = useState(false);
   const [nickName, setNickname] = useState("");
   const [infoText, setInfoText] = useState("");
 
@@ -32,6 +34,7 @@ function LandingPage() {
 
   useSubscription("/user/queue/random-name", (message) => {
     setNickname(message.body);
+    setIsConnected(true);
   });
 
   useSubscription("/user/queue/login/user-name", (message) => {
@@ -87,71 +90,74 @@ function LandingPage() {
     const regex = new RegExp("^[a-zA-Z].*$");
     return regex.test(input);
   }
-
-  return (
-    <motion.div
-      className="relative flex flex-col items-center"
-      initial={{ opacity: 0, scale: 0 }}
-      animate={{ opacity: 1, scale: 1 }}
-      transition={{ type: "spring" }}
-    >
-      {showPrivacyPolicy && (
-        <PrivacyPolicy
-          setShowPrivacyPolicy={setShowPrivacyPolicy}
-        ></PrivacyPolicy>
-      )}
-      {infoText !== "" && (
-        <p className="absolute -top-10 w-max animate-bounce rounded border border-game-accent-light bg-game-accent-medium p-2 text-game-main-light">
-          {infoText}
-        </p>
-      )}
-      <form
-        className="flex w-60 flex-col rounded bg-game-accent-light p-2"
-        onSubmit={(e) => tryLogin(e)}
+  if (isConnected) {
+    return (
+      <motion.div
+        className="relative flex flex-col items-center"
+        initial={{ opacity: 0, scale: 0 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ type: "spring" }}
       >
-        <div className="flex flex-col rounded bg-game-accent-medium p-1">
-          <label className="pl-1" htmlFor="nickname">
-            Nickname
-          </label>
-          <input
-            className="w-full bg-game-main-dark pl-1"
-            type="text"
-            name="nickname"
-            id="nickname"
-            onChange={(e) => setNickname(e.currentTarget.value)}
-            value={nickName}
-          />
-        </div>
-        <div className="flex justify-between">
-          <MotionButton
-            className="mt-3 rounded bg-game-accent-medium px-4 py-1"
-            onClick={requestRandomName}
-          >
-            Generate
-          </MotionButton>
-          <MotionButton
-            className="mt-3 rounded bg-game-accent-medium px-4 py-1"
-            type="submit"
-          >
-            Continue
-          </MotionButton>
-        </div>
-      </form>
-      <p className="absolute top-28 mt-2 w-56 text-center text-xs text-game-main-light">
-        By continuing, you confirm that you have read and understood the{" "}
-        <span className="">
-          <button
-            className="underline hover:text-game-accent-light"
-            type="button"
-            onClick={() => setShowPrivacyPolicy(true)}
-          >
-            Privacy Policy
-          </button>
-        </span>
-        .
-      </p>
-    </motion.div>
-  );
+        {showPrivacyPolicy && (
+          <PrivacyPolicy
+            setShowPrivacyPolicy={setShowPrivacyPolicy}
+          ></PrivacyPolicy>
+        )}
+        {infoText !== "" && (
+          <p className="absolute -top-10 w-max animate-bounce rounded border border-game-accent-light bg-game-accent-medium p-2 text-game-main-light">
+            {infoText}
+          </p>
+        )}
+        <form
+          className="flex w-60 flex-col rounded bg-game-accent-light p-2"
+          onSubmit={(e) => tryLogin(e)}
+        >
+          <div className="flex flex-col rounded bg-game-accent-medium p-1">
+            <label className="pl-1" htmlFor="nickname">
+              Nickname
+            </label>
+            <input
+              className="w-full bg-game-main-dark pl-1"
+              type="text"
+              name="nickname"
+              id="nickname"
+              onChange={(e) => setNickname(e.currentTarget.value)}
+              value={nickName}
+            />
+          </div>
+          <div className="flex justify-between">
+            <MotionButton
+              className="mt-3 rounded bg-game-accent-medium px-4 py-1"
+              onClick={requestRandomName}
+            >
+              Generate
+            </MotionButton>
+            <MotionButton
+              className="mt-3 rounded bg-game-accent-medium px-4 py-1"
+              type="submit"
+            >
+              Continue
+            </MotionButton>
+          </div>
+        </form>
+        <p className="absolute top-28 mt-2 w-56 text-center text-xs text-game-main-light">
+          By continuing, you confirm that you have read and understood the{" "}
+          <span className="">
+            <button
+              className="underline hover:text-game-accent-light"
+              type="button"
+              onClick={() => setShowPrivacyPolicy(true)}
+            >
+              Privacy Policy
+            </button>
+          </span>
+          .
+        </p>
+      </motion.div>
+    );
+  } else {
+    return <ErrorPopUp mainText="Connecting to the server." />;
+  }
 }
 
 export default LandingPage;
